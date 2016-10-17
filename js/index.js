e4d741c70a8d135f648a5921cc3e111fe06a2a0d
helious <rangemonk@gmail.com>
2016-10-17 19:40:55 -0400

    Completed desktop experience.

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 window.angular
-    .module('grayjax', ['ngRoute'])
+    .module('grayJax', ['ngRoute', 'ui.mask'])
     .config([
         '$routeProvider',
         '$locationProvider',
@@ -9,6 +9,7 @@ window.angular
             $routeProvider
                 .when('/', {
                     pageId: 'home',
+                    controller: 'Home',
                     templateUrl: '/views/home.nghtml'
                 })
                 .when('/products', {
@@ -26,13 +27,41 @@ window.angular
     ])
     .controller('GrayJaxRoot', [
         '$scope',
-        function GrayJaxRootController($scope) {
+        '$sce',
+        function GrayJaxRootController($scope, $sce) {
+            var CONTACT_US_EMAIL_ADDRESS = 'rangemonk@gmail.com';
+
+            $scope.contactUsFormUrl = $sce.trustAsResourceUrl('https://formspree.io/' + CONTACT_US_EMAIL_ADDRESS);
             $scope.contactUsRequest = {}
+
+            $scope.contactUs = function contactUs($event) {
+                if (!$scope.contactUsForm.$valid) {
+                    $event.preventDefault();
+
+                    $scope.contactUsForm.$setSubmitted();
+
+                    return;
+                }
+            }
 
             $scope.$on('$routeChangeSuccess', function onRouteChangeSuccess(event, currentRoute) {
                 $scope.contactUsForm.$setUntouched();
-console.log($scope.contactUsForm);
+
                 $scope.pageId = currentRoute.pageId;
             });
+        }
+    ])
+    .controller('Home', [
+        '$scope',
+        '$routeParams',
+        '$timeout',
+        function HomeController($scope, $routeParams, $timeout) {
+            if ($routeParams.hasContactedUs === 'true') {
+                $scope.hasContactedUs = true;
+
+                $timeout(function onTimeout() {
+                    $scope.hideContactedUsBanner = true;
+                }, 9667);
+            }
         }
     ]);
